3e884fcd5b26639dff57840e3f2f3a323e0b3d08
HyeonbellU <cricket1026@gmail.com>
2025-12-02 16:48:51 +0900

    refactor: one line if comment

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,6 @@
 # 16~30의 fizzbuzz
 for i in range(16,30+1):
-    if i%15==0:
-        print('fizzbuzz')
-    elif i%3==0:
-        print('fizz')
-    elif i%5==0:
-        print('buzz')
+    if i%3==0 or i%5==0:
+        print('fizz'*(i%3==0)+'buzz'*(i%5==0))
     else:
         print(i)
